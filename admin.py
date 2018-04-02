732d60c7e946c8ba56690d813288f635e1e172b4
Alejo1021 <valejandro12@hotmail.com>
2018-04-02 17:26:08 -0500

    agego un archivo

---
+++ admin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from .models import Product
+from django.contrib import admin
+
+admin.site.register(Product)
+
+# Register your models here.
